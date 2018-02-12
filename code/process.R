aa9e62da222702d25f9e8dc64a4479a98483e3fe
Trevor Harris <trevorh2@illinois.edu>
2018-02-12 13:09:43 -0600

    added a personal tasklist, the presentation from 2/9, and started comparing looped versions of the code

---
+++ code/process.R
@@ -0,0 +1,120 @@
+rm(list = ls())
+gc()
+
+library(extdepth)
+library(ncdf4)
+library(dplyr)
+library(plotly)
+library(RcppEigen)
+
+##### CONNECT TO DATA #####
+# read ensembles and prior ncdf4 objects
+nc.ens = nc_open('/Users/trevh/Research/assimilation-cfr/data/tas_ens_da_hydro_r.1000-2000_d.30-Nov-2017.nc')
+nc.prior = nc_open('/Users/trevh/Research/assimilation-cfr/data/tas_prior_da_hydro_r.1000-2000_d.30-Nov-2017.nc')
+
+# get data dim
+n.lon = nc.ens$dim$lon$len
+n.lat = nc.ens$dim$lat$len
+n.ens = nc.ens$dim$sub_ens$len
+
+
+
+##### CONSTRUCT THE BASIS FUNCTIONS #####
+nlat.centers = 10
+nlon.centers = 15
+n.center = nlat.centers * nlon.centers
+
+lats = as.vector(nc.ens$dim$lat$vals)
+lons = as.vector(nc.ens$dim$lon$vals)
+lats.cen = seq(min(lats), max(lats), length.out = nlat.centers)
+lons.cen = seq(min(lons), max(lons), length.out = nlon.centers)
+
+# find the radius (1.5 times the minimal distance)
+# will either be the distance b/w the first point and the next (horizontally or vertically)
+rad = 1.5 * min(abs(lats.cen[1] - lats.cen[2]), abs(lons.cen[1] - lons.cen[2]))
+
+# CHECK THAT THIS LINES UP WITH X
+latlon = as.matrix(expand.grid(1:nlat.centers, 1:nlon.centers))
+basis = matrix(0, n.lat*n.lon, n.center)
+
+for (i in 1:n.center) {
+  
+  # dist = (lat - center.lat)^2 + (lon - center.lon)^2
+  dist = c(outer((lats - lats.cen[latlon[i,][1]])^2, (lons - lons.cen[latlon[i,][2]])^2, "+"))
+  
+  # use the bisquare (radial) basis
+  basis[,i] = (1 - dist/rad^2)^2 * (sqrt(dist) < rad)
+}
+
+
+##### PREPROCESS THE FIELDS #####
+# eventually it'll be a loooop
+t = 1
+
+# extract data from the ncdf4 objects
+ens = ncvar_get(nc.ens, attributes(nc.ens$var)$names[1], start = c(1, 1, t, 1), count = c(-1, -1, 1, -1))
+prior = ncvar_get(nc.prior, attributes(nc.prior$var)$names[1], start = c(1, 1, t), count = c(-1, -1, 1))
+
+# transpose for intuitive (to me) layout
+ens = aperm(ens, c(2, 1, 3))
+prior = t(prior)
+
+# remove lat means
+ens = vapply(1:n.ens, function(x) ens[,,x] - rowMeans(ens[,,x]), FUN.VALUE = matrix(0, nrow = n.lat, ncol = n.lon))
+prior = prior - rowMeans(prior)
+
+# normalize
+lats = as.vector(nc.ens$dim$lat$vals)
+latmat = matrix(rep(lats, n.lon), n.lat, n.lon)
+latmat = sqrt(abs(cos(latmat*pi/180)))
+
+ens = vapply(1:n.ens, function(x) ens[,,x]*latmat, FUN.VALUE = matrix(0, nrow = n.lat, ncol = n.lon))
+prior = prior * latmat
+
+
+##### FIT BASIS AND FIND ED #####
+prior.alpha = coef(fastLmPure(basis, as.vector(prior)))
+ens.alpha = sapply(1:n.ens, function(x) coef(fastLmPure(basis, as.vector(ens[,,x]))))
+
+# calculate extremal depth
+ed = sapply(1:ncol(ens.alpha), function(x) edepth(ens.alpha[,x], ens.alpha))
+central = central_region(ens.alpha, ed)
+
+
+##### DIAGNOSTICS #####
+# is the prior within the CR?
+plot(prior.alpha, type = "l")
+polygon(c(1:n.center, rev(1:n.center)),
+        c(central[[2]], rev(central[[1]])),
+        col="skyblue",
+        border = NA)
+
+# lines(central[[1]])
+# lines(central[[2]])
+
+
+# # what do the ens coeff look like
+# plot(ens.alpha[,1], type = "l")
+# for (i in 2:100) {
+#   lines(ens.alpha[,i])
+# }
+# # plus prior
+# lines(prior.alpha, col = "red")
+# 
+# # check the smoothed approximation
+# prior.smooth = matrix(fastLm(basis, as.vector(prior))$fitted.values, 96, 144)
+# 
+# plot_ly(showscale = FALSE) %>% 
+#   add_surface(z = ~prior)
+# 
+# plot_ly(showscale = FALSE) %>% 
+#   add_surface(z = ~prior.smooth)
+
+
+
+
+
+
+
+
+
---
+++ notes/Tasks.md
@@ -0,0 +1,22 @@
+# Tasks
+
+- Implement basis function system
+  1. ~~Scrap the Eigendecomp method~~
+  2. ~~Create radial (bisquare) basis functions~~
+     1. Check that these are accurate and everything lines up (semi done)
+- Implement a (modified) version of Extremal data depth for handling noisy functional data
+  1. ~~Implement standard ED~~
+     1. Test these against easy functions (semi done)
+  2. ~~Implement central regions~~
+     1. Test these against easy functions (semi done)
+  3. Modify ED to handle points being slightly outside 
+     1. Ask Naveen for papers related to this
+     2. Setup meeting with Naveen and Bo 
+     3. Think about what sort of tolerances should be allowed
+- Compare ensemble with prior
+  - What should results here look like? Graphs? a summary statistic? Check the Smerdon paper
+- Compare ensemble with target ensemble
+  - Need to get data for this
+  - Implement or find FDR methods for these sort of tests
+  - Same as above
+
