1144bd053fe312c85f1bad06609977445513d72b
Trevor Harris <trevorh2@illinois.edu>
2018-06-27 20:53:49 -0600

    updated posterior mean

--- sim_study/power/mean/code/generate_post_mu.R
+++ sim_study/power/mean/code/generate_post_mu.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 library(reshape2)
-source('research/assimilation-cfr/sim_study/shared_code/gaussian_process.R')
+source('sim_study/shared_code/gaussian_process.R')
+
 
 set.seed(420)
 post_mu = sim_gp(1, mu = 0, l = 10, 40)
@@ -15,7 +16,5 @@ ggplot(data = mu_df, aes(x = Var2, y = Var1)) +
 
 ggsave(paste0("sim_study/power/mean/plots/post_mu.png"), width = 5, height = 3.2)
 
-saveRDS(matrix(post_mu, 40, 40), file = paste0("/Users/trevh/research/assimilation-cfr/sim_study/power/mean/data/post_mu.rds"))
-
-
+saveRDS(matrix(post_mu, 40, 40), file = "sim_study/power/mean/data/post_mu.rds")
 
