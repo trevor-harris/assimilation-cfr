13ed1d11989c1d465a014d8f6739713f20c8cb81
Trevor Harris <trevor@DESKTOP-QQMS44S.localdomain>
2018-08-20 09:06:41 -0500

    added in new graphs

--- New/analysis.R
+++ New/analysis.R
@@ -5,6 +5,7 @@ library(extdepth)
 library(ncdf4)
 library(dplyr)
 library(reshape2)
+library(ggplot2)
 library(OpenImageR)
 
 # prepare data
@@ -56,7 +57,6 @@ prep_post = function(nc.post, t) {
   
   return(ens)
 }
-
 flatten = function(mat) {
   matrix(mat, prod(dim(mat)[1:2]), dim(mat)[3])
 }
@@ -94,37 +94,101 @@ ks_pval = function(t, n = 20) {
   2*(sum(sapply(1:n, function(x) (-1)^(x-1) * exp(-2*(x^2)*t^2))))
 }
 
-# read ensembles and prior ncdf4 objects
-# nc.post = nc_open('/Users/trevh/research/assimilation-cfr/data/tas_ens_da_hydro_r.1000-2000_d.16-Feb-2018.nc')
-# nc.prior = nc_open('/Users/trevh/research/assimilation-cfr/data/tas_prior_da_hydro_r.1000-2000_d.16-Feb-2018.nc')
+pvals = read.csv("../research/assimilation-cfr/cfr/adjusted_pvals")$x
+
+signif = as.numeric(pvals < 0.05)
+time = 1:998
+signif.model = glm(signif ~ time, family="binomial")
+
+plot(signif)
+lines(signif.model$fitted.values)
+pexp = -log10(pvals)
+plot(pexp)
 
 
+# inspect some of the times with the bigggest differences
 nc.post = nc_open('../research/climate_data/tas_ens_da_hydro_r.1000-2000_d.16-Feb-2018.nc')
 nc.prior = nc_open('../research/climate_data/tas_prior_da_hydro_r.1000-2000_d.16-Feb-2018.nc')
 
+# smallest differences
+time = 2
 prior = prep_prior(nc.prior)
+post = prep_post(nc.post, time)
 
-# times = as.integer(seq(1, 998, length.out = 20))
-times = 1:998
-ksp = rep(0, length(times))
+prior = sapply(1:dim(prior)[3], function(x) down_sample_image(prior[,,x], 2))
+post = sapply(1:dim(post)[3], function(x) down_sample_image(post[,,x], 2))
 
-for(t in 1:length(times)) {
-  post = prep_post(nc.post, times[t])
+prior.depths = meandepth(prior, prior)
+post.depths = meandepth(post, prior)
+
+prior.surv = rev(c(0, sort(prior.depths)))
+post.cdf = sapply(1:length(prior.surv), function(x) mean(post.depths > prior.surv[x]))
+prior.cdf = sapply(1:length(prior.surv), function(x) mean(prior.depths > prior.surv[x]))
+
+cdfgg = data.frame(prior = prior.cdf, posterior = post.cdf)
+cdfgg = melt(cdfgg)
+cdfgg["depth"] = rep(seq(0, 1, length.out = 999), 2)
+
+ggplot(cdfgg) +
+  geom_line(
+    aes(
+      x = depth,
+      y = value,
+      color = variable),
+    size = 0.9) +
+  theme_classic() +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  ggtitle(paste("Year ", time)) +
+  xlab("1 - Depth")
+
+
+# medium differences
+time = 100
+post = prep_post(nc.post, time)
+post = sapply(1:dim(post)[3], function(x) down_sample_image(post[,,x], 2))
+
+post.depths = meandepth(post, prior)
+post.cdf = sapply(1:length(prior.surv), function(x) mean(post.depths > prior.surv[x]))
+
+cdfgg = data.frame(prior = prior.cdf, posterior = post.cdf)
+cdfgg = melt(cdfgg)
+cdfgg["depth"] = rep(seq(0, 1, length.out = 999), 2)
+
+ggplot(cdfgg) +
+  geom_line(
+    aes(
+      x = depth,
+      y = value,
+      color = variable),
+    size = 0.9) +
+  theme_classic() +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  ggtitle(paste("Year ", time)) +
+  xlab("1 - Depth")
+
+
+# biggest difference
+time = which.min(pvals)
+post = prep_post(nc.post, time)
+post = sapply(1:dim(post)[3], function(x) down_sample_image(post[,,x], 2))
+
+post.depths = meandepth(post, prior)
+post.cdf = sapply(1:length(prior.surv), function(x) mean(post.depths > prior.surv[x]))
+
+cdfgg = data.frame(prior = prior.cdf, posterior = post.cdf)
+cdfgg = melt(cdfgg)
+cdfgg["depth"] = rep(seq(0, 1, length.out = 999), 2)
+
+ggplot(cdfgg) +
+  geom_line(
+    aes(
+      x = depth,
+      y = value,
+      color = variable),
+    size = 0.9) +
+  theme_classic() +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  ggtitle(paste("Year ", time)) +
+  xlab("1 - Depth")
   
-  # apply low pass filter to remove "noise" (really just to speed it up)
-  prior.t = sapply(1:dim(prior)[3], function(x) down_sample_image(prior[,,x], 4))
-  post.t = sapply(1:dim(post)[3], function(x) down_sample_image(post[,,x], 4))
-  
-  # test
-  ksp[t] = ks.mean(prior.t, post.t)
-  cat(times[t], ":", ksp[t], "\n")
-}
 
-ksp.adj = p.adjust(ksp, n = 998)
-
-plot(ksp.adj)
-
-summary(ksp.adj)
-
-write.csv(ksp, "../research/assimilation-cfr/cfr/pvals")
-write.csv(ksp.adj, "../research/assimilation-cfr/cfr/adjusted_pvals")
