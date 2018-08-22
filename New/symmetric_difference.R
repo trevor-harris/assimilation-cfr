25fd75c770f9ef89adbc5d9a4205797e6e0dfbe4
Trevor Harris <trevor@DESKTOP-QQMS44S.localdomain>
2018-08-22 11:47:32 -0500

    started 2D function testing

--- New/symmetric_difference.R
+++ New/symmetric_difference.R
@@ -2,12 +2,10 @@ rm(list = ls()); gc()
 library(extdepth)
 library(tictoc)
 
-
-#### SIMULATION ####
-gp2d = function(fields = 100, mu = 0, l = 30, pts = 30) {
+# sims
+gp1d = function(fields = 100, mu = 0, l = 50, pts = 50) {
   
   grid = 1:pts
-  grid = expand.grid(grid, grid)
   distmat = as.matrix(dist(grid))
   
   # calc sigma with cov kernel
@@ -16,9 +14,9 @@ gp2d = function(fields = 100, mu = 0, l = 30, pts = 30) {
   sigma.eig = eigen(sigma)
   sigma.half = sigma.eig$vectors %*% diag(sqrt(sigma.eig$values)) %*% t(sigma.eig$vectors)
   
-  gps = array(0, dim=c(pts, pts, fields))
+  gps = matrix(0, pts, fields)
   for(f in 1:fields) {
-    gps[,,f] = (sigma.half %*% rnorm(pts^2)) + mu
+    gps[,f] = (sigma.half %*% rnorm(pts)) + mu
   }
   return(gps)
 }
@@ -32,10 +30,6 @@ plt_funs = function(f, g) {
   for(i in 1:ncol(g)) {
     lines(g[,i], col = "blue")
   }
-}
-
-flatten = function(mat) {
-  matrix(mat, prod(dim(mat)[1:2]), dim(mat)[3])
 }
 
 # depth CDF
@@ -55,44 +49,56 @@ depth = function(g, fmat) {
 meandepth = function(gmat, fmat) {
   apply(gmat, 2, function(x) mean(depth(x, fmat)))
 }
-ks.mean = function(f, g) {
-  fed = meandepth(f, f)
-  ged = meandepth(g, f)
-  
-  f.surv = rev(c(0, sort(fed)))
-  g.cdf = sapply(1:length(f.surv), function(x) mean(ged > f.surv[x]))
-  f.cdf = sapply(1:length(f.surv), function(x) mean(fed > f.surv[x]))
-  
-  ks = max(abs(f.cdf - g.cdf))
-  rate = sqrt((ncol(g)*ncol(f)) / (ncol(g) + ncol(f)))
-  ks_pval(rate*ks)
-}
-ks_pval = function(t, n = 5) {
+ks_pval = function(t, n = 20) {
   2*(sum(sapply(1:n, function(x) (-1)^(x-1) * exp(-2*(x^2)*t^2))))
 }
+
+joint.ks = function(f, g) {
+  ff = meandepth(f, f)
+  fg = meandepth(f, g)
+  gf = meandepth(g, f)
+  gg = meandepth(g, g)
+  
+  ffr = rank(ff) / length(ff)
+  fgr = rank(fg) / length(fg)
+  gfr = rank(gf) / length(gf)
+  ggr = rank(gg) / length(gg)
+  
+  
+  alpha = seq(1, 0, length.out = 1000)
+  jcdf = sapply(alpha, function(t) mean(c(((ffr > t) & (fgr > t)), ((ggr > t) & (gfr > t)))))
+  
+  ks = max(abs(1-alpha - jcdf))
+  rate = sqrt(sqrt(2)*length(jcdf))
+  ks_pval(rate*ks)
+}
+
+
+f = gp1d(500, mu = 0, pts = 50, l = 10)
+g = gp1d(500, mu = 0, pts = 50, l = 10)
+
+joint.ks(f, g)
+
+
 
 #### SIZE
 set.seed(1)
 
-sims = 500
-kmain = rep(0, sims)
+sims = 2000
+kdist = rep(0, sims)
 for(s in 1:sims) {
   tic("Total")
   cat("Simulation ", s, "\n")
   
-  gp1 = gp2d(500, mu = 0, pts = 30, l = 10)
-  gp2 = gp2d(500, mu = 0, pts = 30, l = 10)
+  gp1 = gp1d(500, mu = 0, pts = 50, l = 10)
+  gp2 = gp1d(500, mu = 0, pts = 50, l = 10)
   
-  gp1 = flatten(gp1)
-  gp2 = flatten(gp2)
+  kdist[s] = joint.ks(gp1, gp2) 
   
-  kmain[s] = ks.mean(gp1, gp2)
-  
-  cat("Size: ", mean(kmain[1:s] < 0.05), "\n")
   toc()
   cat("\n")
 }
-mean(kmain < 0.05)
-plot(kmain)
+mean(kdist < 0.05)
+plot(kdist)
 
-# plt_funs(gp1, gp2)
+plt_funs(gp1, gp2)
